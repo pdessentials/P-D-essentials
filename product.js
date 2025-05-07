20f64df2179eb86881f0eaa43e5711a07382c909
pdessentials <pdessentials26632@gmail.com>
2025-05-07 21:05:24 +0530

    Update and rename add-product.js to product.js

---
+++ product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router();
+
+// Get all products
+router.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// Add new product
+router.post('/', async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.status(201).json(product);
+});
+
+module.exports = router;
+
+---
